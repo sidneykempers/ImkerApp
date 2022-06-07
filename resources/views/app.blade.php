c4f7af21f7b21dfba0b3440c4261d82c60b4ccfe
Aydin0509 <6004081@mborijnland.nl>
2022-06-07 09:24:28 +0200

    header

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,11 +3,26 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
+
+<img id="HomeBanner" src="images/bij.jpg" alt="bij banner">
+<header>
+    <nav>
+        <ul class="main_menu">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Basiscursus</a></li>
+            <li><a href="#">Voortgezet Imkeren</a></li>
+            <li><a href="#">Informatie</a></li>
+            <li><a href="#">Links</a></li>
+            <li><a href="#">Contact</a></li>
+            <li><a href="#">Berichten</a></li>
+        </ul>
+    </nav>
+</header>
 <body>
 <h1 class="text-3xl font-bold underline">
-    Hello world!
 </h1>
 </body>
 </html>
