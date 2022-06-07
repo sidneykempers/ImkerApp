f1574aa2a1bf7e47c4746fe75629a3b5820627b7
sidneykempers <6008573@mborijnland.nl>
2022-06-07 10:01:16 +0200

    Homepage aangepast

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,25 +4,30 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
 </head>
+<div class="container">
+    <div id="center" >
+        <img id="HomeBanner" src="images/bij.jpg" alt="bij banner">
+    </div>
 
-<img id="HomeBanner" src="images/bij.jpg" alt="bij banner">
-<header>
-    <nav>
-        <ul class="main_menu">
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Basiscursus</a></li>
-            <li><a href="#">Voortgezet Imkeren</a></li>
-            <li><a href="#">Informatie</a></li>
-            <li><a href="#">Links</a></li>
-            <li><a href="#">Contact</a></li>
-            <li><a href="#">Berichten</a></li>
-        </ul>
-    </nav>
-</header>
-<body>
-<h1 class="text-3xl font-bold underline">
-</h1>
-</body>
+    <header>
+        <nav>
+            <ul class="main_menu">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Basiscursus</a></li>
+                <li><a href="#">Voortgezet Imkeren</a></li>
+                <li><a href="#">Informatie</a></li>
+                <li><a href="#">Links</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="#">Berichten</a></li>
+            </ul>
+        </nav>
+    </header>
+    <body>
+    <h1 class="text-3xl font-bold underline">
+    </h1>
+    </body>
+</div>
+
 </html>
